4d06f37db26e125158b3b4e8a5ebbe0c611a2705
bingzz <bingz.real@gmail.com>
2023-08-30 10:47:05 +0800

    Added sidebar navigation
    Added Logout button Icon

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter, map } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
@@ -7,15 +8,34 @@ import { Router } from '@angular/router';
   styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  activePage: string
 
-  constructor(private router: Router) { }
+  constructor(private router: Router) {
+    this.activePage = router.url.split('/').pop() ?? ''
+  }
+
+  navigateToPage(url: string) {
+    if (!url) return
+
+    this.activePage = url
+    this.router.navigate([`/home/${url}`])
+  }
+
+  setCurrentPage(path: string): string {
+    return path === this.activePage ? 'primary' : ''
+  }
 
   logout() {
-    console.log('logout');
-
+    // Clear cache
     this.router.navigate(['/'])
   }
 
-  ngOnInit() { }
-
+  ngOnInit() {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(() => this.router.url.split('/').pop())
+    ).subscribe(currentPath => {
+      this.activePage = currentPath ?? ''
+    })
+  }
 }
